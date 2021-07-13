5c29056aa11bc1d7d4eb8c27f0e1dbf149f89e32
guygot1 <guygot1@gmail.com>
2021-07-13 11:41:44 +0300

    full project

---
+++ OperationSystemsAss2-main/kernel/sysproc.c
@@ -0,0 +1,139 @@
+#include "types.h"
+#include "riscv.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "proc.h"
+
+uint64
+sys_exit(void)
+{
+  int n;
+  if(argint(0, &n) < 0)
+    return -1;
+  exit(n);
+  return 0;  // not reached
+}
+
+uint64
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+uint64
+sys_fork(void)
+{
+  return fork();
+}
+
+uint64
+sys_wait(void)
+{
+  uint64 p;
+  if(argaddr(0, &p) < 0)
+    return -1;
+  return wait(p);
+}
+
+uint64
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+uint64
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+uint64
+sys_kill(void)
+{
+  int pid;
+  int signum;
+  if(argint(0, &pid) < 0)
+    return -1;
+  if((argint(1, &signum) < 0) || signum < 0 || signum > 31 )
+    return -1;  
+  return kill(pid,signum);
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+uint64
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+uint64
+sys_sigprocmask(void){
+  int sigmask;
+  if(argint(0, &sigmask) < 0)
+    return -1;
+  return sigprocmask(sigmask);
+}
+
+
+
+uint64
+sys_sigaction(void){
+  int signum;
+  struct sigaction *act;
+  struct sigaction *oldact;
+    if(argint(0, &signum) < 0 || signum > 31 || signum < 0 ||signum==SIGSTOP ||signum == SIGKILL)
+      return -1;  
+    if(argaddr(1,(void*)&act)<0)
+      return -1;
+    if(argaddr(2,(void*)&oldact)<0)
+      return -1;   
+      
+  
+  return sigaction(signum,act,oldact);
+
+{
+  /* data */
+};
+
+
+
+}
+
+uint64 
+sys_sigret(void){
+  sigret();
+  //in fail return -1 ..???
+  return 0;
+}
